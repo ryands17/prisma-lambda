82bee3f91533835c91f1eb9e491922a104e3c61a
Ryan <dsouza.ryan.11@gmail.com>
2021-03-02 21:14:44 +0530

    Initial commit

---
+++ bin/prisma-lambda.ts
@@ -0,0 +1,7 @@
+#!/usr/bin/env node
+import 'source-map-support/register';
+import * as cdk from '@aws-cdk/core';
+import { PrismaLambdaStack } from '../lib/prisma-lambda-stack';
+
+const app = new cdk.App();
+new PrismaLambdaStack(app, 'PrismaLambdaStack');
