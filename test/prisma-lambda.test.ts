70ccbb6436498ae8d1a15c344440bd987386579f
Ryan <dsouza.ryan.11@gmail.com>
2021-03-03 01:21:37 +0530

    add snapshot tests and deps management

--- test/prisma-lambda.test.ts
+++ test/prisma-lambda.test.ts
@@ -1,13 +1,14 @@
-// import { expect as expectCDK, matchTemplate, MatchStyle } from '@aws-cdk/assert';
-// import * as cdk from '@aws-cdk/core';
-// import * as PrismaLambda from '../lib/prisma-lambda-stack';
+import { SynthUtils } from '@aws-cdk/assert'
+import * as cdk from '@aws-cdk/core'
+import * as PrismaLambda from '../lib/prisma-lambda-stack'
 
-// test('Empty Stack', () => {
-//     const app = new cdk.App();
-//     // WHEN
-//     const stack = new PrismaLambda.PrismaLambdaStack(app, 'MyTestStack');
-//     // THEN
-//     expectCDK(stack).to(matchTemplate({
-//       "Resources": {}
-//     }, MatchStyle.EXACT))
-// });
+test('Snapshot matches correctly', () => {
+  const stack = createStack({ outdir: 'cdk.out', stackTraces: false })
+  const snapshot = SynthUtils.synthesize(stack)
+  expect(snapshot).toMatchSnapshot()
+})
+
+function createStack(props?: cdk.AppProps) {
+  const app = new cdk.App(props)
+  return new PrismaLambda.PrismaLambdaStack(app, 'MyTestStack')
+}
