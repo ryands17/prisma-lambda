5c437c58542982c5fdbe4fd8c8e2c0f7a48708a8
Ryan <dsouza.ryan.11@gmail.com>
2021-03-02 21:22:12 +0530

    initial commit

--- bin/prisma-lambda.ts
+++ bin/prisma-lambda.ts
@@ -1,7 +1,9 @@
 #!/usr/bin/env node
-import 'source-map-support/register';
-import * as cdk from '@aws-cdk/core';
-import { PrismaLambdaStack } from '../lib/prisma-lambda-stack';
+import 'source-map-support/register'
+import * as cdk from '@aws-cdk/core'
+import { PrismaLambdaStack } from '../lib/prisma-lambda-stack'
 
-const app = new cdk.App();
-new PrismaLambdaStack(app, 'PrismaLambdaStack');
+const app = new cdk.App()
+new PrismaLambdaStack(app, 'PrismaLambdaStack', {
+  env: { region: app.node.tryGetContext('region') },
+})
--- test/prisma-lambda.test.ts
+++ test/prisma-lambda.test.ts
@@ -1,13 +1,13 @@
-import { expect as expectCDK, matchTemplate, MatchStyle } from '@aws-cdk/assert';
-import * as cdk from '@aws-cdk/core';
-import * as PrismaLambda from '../lib/prisma-lambda-stack';
+// import { expect as expectCDK, matchTemplate, MatchStyle } from '@aws-cdk/assert';
+// import * as cdk from '@aws-cdk/core';
+// import * as PrismaLambda from '../lib/prisma-lambda-stack';
 
-test('Empty Stack', () => {
-    const app = new cdk.App();
-    // WHEN
-    const stack = new PrismaLambda.PrismaLambdaStack(app, 'MyTestStack');
-    // THEN
-    expectCDK(stack).to(matchTemplate({
-      "Resources": {}
-    }, MatchStyle.EXACT))
-});
+// test('Empty Stack', () => {
+//     const app = new cdk.App();
+//     // WHEN
+//     const stack = new PrismaLambda.PrismaLambdaStack(app, 'MyTestStack');
+//     // THEN
+//     expectCDK(stack).to(matchTemplate({
+//       "Resources": {}
+//     }, MatchStyle.EXACT))
+// });
