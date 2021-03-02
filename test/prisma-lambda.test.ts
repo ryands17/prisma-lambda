641434cd9d0959dd4127c8796371f47f6f572ddf
Ryan <dsouza.ryan.11@gmail.com>
2021-03-03 01:27:15 +0530

    fix tests

--- test/prisma-lambda.test.ts
+++ test/prisma-lambda.test.ts
@@ -1,11 +1,25 @@
-import { SynthUtils } from '@aws-cdk/assert'
+import { expect as expectCDK, haveResourceLike } from '@aws-cdk/assert'
 import * as cdk from '@aws-cdk/core'
 import * as PrismaLambda from '../lib/prisma-lambda-stack'
 
 test('Snapshot matches correctly', () => {
   const stack = createStack({ outdir: 'cdk.out', stackTraces: false })
-  const snapshot = SynthUtils.synthesize(stack)
-  expect(snapshot).toMatchSnapshot()
+
+  expectCDK(stack).to(
+    haveResourceLike('AWS::Lambda::Function', {
+      Code: {
+        S3Bucket: {},
+        S3Key: {},
+      },
+      Role: {},
+      Environment: {
+        Variables: {},
+      },
+      Handler: 'index.handler',
+      Runtime: 'nodejs14.x',
+      Timeout: 10,
+    })
+  )
 })
 
 function createStack(props?: cdk.AppProps) {
